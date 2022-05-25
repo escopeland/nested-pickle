2d60d61ea05fd988d959eb9b88a86c4bde7c7025
escopeland <escopeland@yahoo.com>
2022-05-24 17:10:58 -0700

    replaced <class Holdings> w/ <class BaseHolding> subclassing <list>: pickle fails

--- nested_pickle.py
+++ nested_pickle.py
@@ -136,7 +136,7 @@ class BaseMeta(type):
         return {'__slots__': ()}
 
     def __new__(cls, name, bases, namespace):
-        if bases:
+        if not dict in bases:
             # Assign an owner if we're at the root of an asset stack
             namespace['owner'] = namespace.pop('owner', None) or name
 
@@ -146,33 +146,58 @@ class BaseMeta(type):
                         owner=name,
                         attrs=namespace.pop('attrs')
                 ))
-                namespace['Holdings'] = type(name + '.Holdings', (Holdings, ),
-                    dict(
-                        owner=name,
-                        holding_type=namespace.pop('holding_type', None)
-                ))
-                namespace['__slots__'] = ('history', 'holdings')
+
+
+                holding_type = namespace.pop('holding_type', None)
+                namespace['Holding'] = (
+                    # type(name.split('.')[0] + '.' + holding_type.__name__,
+                    type(name.split('.')[0] + '.' + holding_type.__name__,
+                        (holding_type, ),  dict(owner=namespace['owner']))
+                    if holding_type else None)
+                # namespace['Holdings'] = type(name + '.Holdings', (Holdings, ),
+                #     dict(
+                #         owner=name,
+                #         holding_type=namespace.pop('holding_type', None)
+                # ))
+
+
+                namespace['__slots__'] = ('history', )
+                # namespace['__slots__'] = ('history', 'holdings')
 
         return super().__new__(cls, name, bases, namespace)
 
-class BaseHolding(metaclass=BaseMeta):
+class BaseHolding(dict, metaclass=BaseMeta):
 
     def __new__(cls):
         self = super().__new__(cls)
         self.history  = cls.History()
-        self.holdings = cls.Holdings()
+        # self.holdings = cls.Holdings()
         return self
+
+    def create(self, label):
+        self[label] = self.__class__.Holding()
 
     def __getstate__(self):
         state = dict(owner=self.__class__.owner)
         state['history']  = self.history.__getstate__()
-        state['holdings'] = self.holdings.__getstate__()
+
+        for k, v in self.items():
+            state[k] = v.__getstate__()
+        # state['holdings'] = self.holdings.__getstate__()
+
         return state
 
     def __setstate__(self, state):
         self.__class__.owner = state.pop('owner')
         self.history.__setstate__(state.pop('history'))
-        self.holdings.__setstate__(state.pop('holdings'))
+
+        self.clear()
+        target = self.__class__.Holding
+        for k, s in state.items():
+            v = target()
+            v.__setstate__(s)
+            self[k] = v
+        # self.holdings.__setstate__(state.pop('holdings'))
 
     def __eq__(self, other):
         return self.__getstate__() == other.__getstate__()
@@ -196,13 +221,20 @@ if __name__ == '__main__':
     h3 = Holding()
     h4 = Holding()
 
-    h1.holdings.create('holding-1')
-    h1.holdings['holding-1'].holdings.create('holding-1')
-    s = h1.__getstate__()
+    # h1.holdings.create('holding-1')
+    # h1.holdings['holding-1'].holdings.create('holding-1')
+    # s = h1.__getstate__()
+    # h1.history.create(label='position-1', x=1, y=2, z=3)
+    # h1.history.create(label='position-2', x=5, y=6, z=7)
+    # h1.holdings['holding-1'].history.create(label='position-1', a=4, b=5)
+    # h1.holdings['holding-1'].holdings['holding-1'].history.create(label='position-1', alpha='alpha', beta='beta', gamma='gamma', delta='delta')
+
+    h1.create('holding-1')
+    h1['holding-1'].create('holding-1')
     h1.history.create(label='position-1', x=1, y=2, z=3)
     h1.history.create(label='position-2', x=5, y=6, z=7)
-    h1.holdings['holding-1'].history.create(label='position-1', a=4, b=5)
-    h1.holdings['holding-1'].holdings['holding-1'].history.create(label='position-1', alpha='alpha', beta='beta', gamma='gamma', delta='delta')
+    h1['holding-1'].history.create(label='position-1', a=4, b=5)
+    h1['holding-1']['holding-1'].history.create(label='position-1', alpha='alpha', beta='beta', gamma='gamma', delta='delta')
 
     print(f'Serdes test ')
     with timer('    Test: sleeping 1/2 second completed'):
